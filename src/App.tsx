326f95fa387cfbf12eb9a5ac7ecaa67eec6b0811
htu6n7yi <jcavalcanti008@gmail.com>
2025-08-23 16:05:53 -0300

    created component TaskItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TaskItem from "./components/component.TaskItem/TaskItem";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +29,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
-}
+     
+{/* chamei o arquivo para mostrar o componente TaskItem */}
 
-export default App
+
+export default App;
---
+++ src/components/component.TaskItem/TaskItem.tsx
@@ -0,0 +1,26 @@
+import styles from "./TaskItem.module.css";
+import { type Task } from "./types";
+
+type props = {
+  task: Task;
+  onToggle: (id: string) => void;
+  onDelete: (id: string) => void;
+};
+
+export default function TaskItem({ task, onToggle, onDelete }: props) {
+  return (
+    <>
+      <div className={styles.container}>
+        <input
+          type="checkbox"
+          checked={task.completed}
+          onChange={() => onToggle(task.id)}
+        />
+        <span>{task.title}</span>
+        <span>({task.category})</span>
+
+        <button onClick={() => onDelete(task.id)}>Excluir</button>
+      </div>
+    </>
+  );
+}
